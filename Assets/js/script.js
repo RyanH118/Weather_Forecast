ed02db666fd152038ac7a7fe3172fa5cf737ce09
ryanh <ryanhinson118@mail.com>
2024-04-14 21:44:23 -0400

    Added open weather api that I will need.

---
+++ Assets/js/script.js
@@ -0,0 +1,3 @@
+// https://api.openweathermap.org/geo/1.0/direct?q={city name}&limit=1&appid=a940d80377acb0b2e23b22339c9ce1ba
+// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=a940d80377acb0b2e23b22339c9ce1ba&units=imperial
+// https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=a940d80377acb0b2e23b22339c9ce1ba&units=imperial
